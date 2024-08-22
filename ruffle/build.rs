c52cfd7633a099c16218f46d1cfd48d78b8e41ae
qqhsx <56535272+qqhsx@users.noreply.github.com>
2024-08-22 11:01:09 +0800

    Delete ruffle directory

--- ruffle/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    // See 'desktop/build.rs' for more information.
-    if std::env::var("TARGET").unwrap().contains("wasm") {
-        println!("cargo:rustc-link-arg=-z");
-        println!("cargo:rustc-link-arg=stack-size=2000000");
-    }
-}
